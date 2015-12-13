42c66e22931e6456b64f6503d72f0c915ed19b8c
emilyinglis <emilyameliainglis@gmail.com>
2015-12-13 15:34:26 +0000

    Updated shopify.js and created a utils.js file following research. Not yet sure how to utilise but have featured link to original to reference / guide later

---
+++ wander-feed-node-app/api/lib/utils.js
@@ -0,0 +1,22 @@
+// Copied from here: https://github.com/typefoo/node-shopify/edit/master/lib/shopify.js
+
+
+module.exports = {
+  // function to clone an object
+  clone: function(obj) {
+    var newobj = {};
+    for(var keys = Object.keys(obj), l = keys.length; l; --l) {
+       newobj[keys[l-1]] = obj[keys[l-1]];
+    }
+    
+    return newobj;
+  },
+  extend: function(a, b) {
+    var newvar = this.clone(a);
+    for(var x in b) {
+      newvar[x] = b[x];
+    }
+    
+    return newvar;
+  }
+}
