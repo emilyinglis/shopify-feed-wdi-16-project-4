5173cfdafe0cdc206b31c983c244a757f82bbea0
emilyinglis <emilyameliainglis@gmail.com>
2015-12-13 15:54:20 +0000

    Updated front end including nav and styling i.e. removed previous styling to create a clean slate

--- wander-feed-node-app/front-end/js/app.js
+++ wander-feed-node-app/front-end/js/app.js
@@ -1,13 +1,13 @@
 angular
-  .module('logging', ['ngResource', 'angular-jwt', 'ui.router', 'satellizer'])
-  .constant('API', 'https://804c38c7e20539c1d5c856675950afae:50e1fc6bd3d62890b1522441599bd42f@wander-feed.myshopify.com/admin/products.json')
+  .module('shopify-feed', ['ngResource', 'ui.router'])
+  // Is this the best format for the API we're using?
+  .constant('API', 'http://localhost:3000/api')  
   .config(MainRouter)
-  .config(function($httpProvider){
-    $httpProvider.interceptors.push('authInterceptor')
-  })
-  .config(function($authProvider) {
-    $authProvider.facebook({ clientId: '1535877196647406'})
-  });
+  // Don't believe this is required:
+  // .config(function($httpProvider){
+  //   $httpProvider.interceptors.push('authInterceptor')
+  // })
+
 
   MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -17,26 +17,14 @@ angular
         url: "/",
         templateUrl: "home.html"
       })
-      .state('login', {
-        url: "/login",
-        templateUrl: "login.html"
+      .state('products', {
+        url: "/products",
+        templateUrl: "products.html",
+        controller: "ProductsController as products"
       })
-      .state('register', {
-        url: "/register",
-        templateUrl: "register.html"
-      })
-      .state('profile', {
-        url: "/profile",
-        templateUrl: "profile.html"
-      })
-      .state('users', {
-        url: "/users",
-        templateUrl: "users.html"
-      })
-      .state('projects', {
-        url: "/projects",
-        templateUrl: "projects.html",
-        controller: "ProjectsController as projects"
+      .state('show', {
+        url: "/products/:id",
+        templateUrl: "show.html"
       })
 
     $urlRouterProvider.otherwise("/");
